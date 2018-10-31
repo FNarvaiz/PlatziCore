2a97318eee2b56c9cf878ef77924f63289782388
Facundo Narvaiz <fan@integrichain.com>
2018-10-31 10:09:42 -0500

    Init

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Platzi_Core.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
